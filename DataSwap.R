536518eeeb3cc27914172a41c2decdf5eb252c40
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2015-10-10 16:41:32 -0700

    continued work on data swapping

--- DataSwap.R
+++ DataSwap.R
@@ -24,17 +24,26 @@ NOT DONE YET
 dswap <- function(ind,sk,sa,sp=1.00) {
    inds <- ind[orderbycols[ind),]
    n <- nrow(inds)
+   blocs <- getblocks(inds,sk)
+}
+
+# since inds has been sorted, records with like keys will be blocked
+# together; this function finds the start and stop row numbers for the
+# blocks
+getblockranges <- functio(inds,sk() {
+   blocks <- NULL
    startblock <- 1
    while (1) {
-      if (startblock >= n) return(inds)
+      # keys combination for this block
+      bloickky <- inds[startblock,sk]
       # find end of this block
       i <- startblock + 1
-      bloickky <- inds[startblock,sk]
       while (1) {
          if (inds[i,sk] != bloickky) {
             endblock <- i - 1
             break
          }
+         blocks <- rbind(blocsk,c(startblock,endblock))
          i <- i + 1
          if (i > n) {
             # have 1-row block at end of data frame, so done
